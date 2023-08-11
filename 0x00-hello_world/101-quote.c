eef6ae8c3fd4e713948ab060e5a05ffa550b970d
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-11 00:50:57 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include <unistd.h>
 /**
- * main - main function
- * Return: 1
-*/
+ * main - Entry point
+ * Return:1
+ */
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1.quo.59)
-return (1);	
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
