470a77c2633fd83a5fb4d73f614f2af3a49136a7
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-10 17:45:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include<stdio.h>
+#include<stdio>
 /**
  * main - main block
  * Return: 0
- */
+*/
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-}	
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
