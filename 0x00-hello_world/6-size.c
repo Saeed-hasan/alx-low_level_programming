f2396934dd2d71d7771a98296928a1f1104eebd0
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-10 18:39:29 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * Return: 0
+*/
+int main(void)
+{
+printf("char size: %d\n" sizeof(char));
+printf("int size: %d\n" sizeof(int));
+printf("long int size: %d\n" sizeof(long int));
+printf("long long int size: %d\n" sizeof(long long int));
+printf("float size: %d\n" sizeof(float));
+return (0);
+}
+
