c8e18c2a9414a8708b2624d25478038531cb3a5a
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-11 17:52:44 -0700

    pandv

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,7 +14,7 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("number is positive\n", n);
 else if (n == 0)
-printf("number is negative\n", n);
+printf("number is zero\n", n);
 else
 printf("number is negative\n", n);
 return (0);
