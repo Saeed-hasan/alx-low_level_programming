89a412709065724562c53d3ce107b5e18f8a29a9
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-11 17:45:12 -0700

    pandv

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * Describtion: postive or negative
+ * Return: 0
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("is positive\n", n);
+else if (n == 0)
+printf("is zero\n", n);
+else
+printf("is negative\n", n);
+return (0);
+}
