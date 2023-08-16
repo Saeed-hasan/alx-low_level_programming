4bc1d9dbe4866c574fcdb2caae44bc46f177950f
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-16 16:43:47 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _abs - return abs value
+ * @n : number to check
+ * Return:0 or 1
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
