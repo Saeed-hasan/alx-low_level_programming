127d7e1b6c96209e308ead6a4805eb1fe9a2ca70
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-16 12:08:04 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * main - entry point
+ * Return:0
+ */
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
