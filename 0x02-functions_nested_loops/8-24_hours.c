4cfcff918e77b398d8a9d427de228d0cb33c4857
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-16 18:35:57 -0700

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ *Jac_kBauer - prints every minute of the day
+ *return:void
+ */
+void jack_bauer(void);
+int min, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(58);
+			_putchar((min / 10) + 48);
+			_putchar((min / 10) + 48);
+			_putchar('\n');
+		}
+	}
