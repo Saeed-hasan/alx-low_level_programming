35a72dad36f2ecb98b8c764bac00790802fb31ce
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-08-17 16:57:52 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _isupper -  checks for uppercase letter
+ * @c : charcter to check the case
+ * Return:0 or 1
+ */
+int _isupper(int c);
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}	
