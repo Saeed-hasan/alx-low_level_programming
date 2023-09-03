3093766582de7cd1ad8bf90b1566efc417df986d
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-09-03 12:45:08 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - prints string
@@ -12,9 +13,9 @@ void _puts(char *str)
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 		i++;
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
