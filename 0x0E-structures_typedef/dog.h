a82c63efd178e811d15eb9609a4fea8a7f89da3b
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-09-11 19:01:21 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - adog struct
+ *@name: dogs name
+ *@age: dogs age
+ *@owner: owners name
+ *Descrition: poppy
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
