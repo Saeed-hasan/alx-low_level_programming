cf949ba9fc9a1aaf4e96396e34a1ff14c30a68fc
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-09-21 16:13:26 -0700

    tasks

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,18 @@
-nasm -f elf64 101-hello_holberton.asm && gcc -no-pie -std=gnu89 101-hello_holberton.o -o hello
+extern printf
+
+	section .data
+msg:		db "Hello, Holberton", 0
+fmt:		db "%s", 10, 0
+
+	section .text
+
+	global main
+main:
+	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+	mov	rax,0
+	ret
