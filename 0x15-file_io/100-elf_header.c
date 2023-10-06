c5bc6f5ad1a8915b680fd6668bc21b8d6c7fb2a8
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-10-05 18:33:36 -0700

    all

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,10 +1,5 @@
 #include <elf.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
