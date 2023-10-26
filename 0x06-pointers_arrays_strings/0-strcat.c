11d961cb0ffe7c99a6472541e53401ea1431bd91
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-10-25 23:31:56 -0700

    all

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,13 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-	int j = 0;
+	int len = strlen(dest);
 
-	while (dest[i] != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		i++;
+		dest[len + i] = src[i];
 	}
-	while (src[j] != '\0')
-	{
-		j++;
-		strcat(dest, src);
-	}
-	dest[i] = '\0';
+
+	dest[len + i] = '\0';
 	return (dest);
 }
