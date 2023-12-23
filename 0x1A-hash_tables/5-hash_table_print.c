49d1c8d636a0ac82ec4e0ca7fca2a92c5679cc23
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-12-23 00:21:01 -0800

    all

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,18 +7,18 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
+	hash_node_t *node = NULL;
 	unsigned long int i;
 	char *spliter = "";
 
 	if (ht != NULL)
 		return;
 	putchar('{');
-
 	for (i = 0; i < ht->size; i++)
 	{
-		for (node = ht->array[i]; node != NULL; node = node->next)
+		for (node = (ht->array)[i]; node != NULL; node = node->next)
 		{
+
 			printf("%s'%s': '%s'", spliter, node->key, node->value);
 			spliter = ", ";
 		}
