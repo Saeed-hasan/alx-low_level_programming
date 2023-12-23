356b00f789a378df98d610eb7f9da7607b16c0df
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-12-23 01:42:16 -0800

    all

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,12 +9,9 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	if (ht == NULL)
-		return;
-
 	for (i = 0; i < ht->size; i++)
 	{
-		for (node = ht->array[i]; node != NULL; node = node->next)
+		for (tmp = ht->array[i]; tmp != NULL; tmp = node->next)
 		{
 			tmp = node->next;
 			free(node->key);
