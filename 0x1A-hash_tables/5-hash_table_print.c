8525bc79ffead0c782d0116994057dd40ee4327e
Saeed-hasan <saeedmohammed1999@gmail.com>
2023-12-22 23:42:14 -0800

    all

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,25 +1,26 @@
 #include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * hash_table_print-print all the hash table
  * @ht:pointer to the hash table
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node = NULL;
+	hash_node_t *node;
 	unsigned long int i;
-	char *spliter = "";
+	char *spliter;
 
-	if (!ht || !ht->array)
+	if (ht != NULL)
 		return;
 	putchar('{');
+
 	for (i = 0; i < ht->size; i++)
 	{
-		node = (ht->array)[i];
-		while (node != NULL)
+		for (node = ht->array[i]; node != NULL; node = node->next)
 		{
-			printf("%s'%s': '%s'", spliter, node->key, node->value);
+			printf("'%s':'%s'%s", node->key, node->value, spliter);
 			spliter = ", ";
-			node = node->next;
 		}
 	}
 	puts("}");
